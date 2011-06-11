2faf190124f681d5e8ef9a34996f819463934b5b
Alejandro Paz <vidriloco@gmail.com>
2011-06-10 21:12:42 -0500

    Spork Rspec y Watcher funcionando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Base::Application.config.secret_token = '4294438b693f6e1dcb9fa9bee8013428115689d7fd833f47e6f8ffbe688f08bf1e2556d8307b9518bd15427a8b093440ab979382f9ae0483ac53cd12ac25a0f6'
